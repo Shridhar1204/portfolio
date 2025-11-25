1ea56b93861a7a0ea369a78bdb1239cf97288716
Shridhar1204 <sharmashridhar1204@gmail.com>
2025-11-25 23:00:01 +0530

    public pdf

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -40,15 +40,13 @@ export default function Greeting() {
               <SocialMedia />
               <div className="button-greeting-div">
                 <Button text="Contact me" href="#contact" />
-                {greeting.resumeLink && (
-                  <a
-                    href={require("./Resume.pdf")}
-                    download="Resume.pdf"
-                    className="download-link-button"
-                  >
-                    <Button text="Download my resume" />
-                  </a>
-                )}
+                <a
+                  href="/Resume.pdf"
+                  download="Resume.pdf"
+                  className="download-link-button"
+                >
+                  <Button text="Download my resume" />
+                </a>
               </div>
             </div>
           </div>
